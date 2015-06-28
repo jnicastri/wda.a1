04b3b3efc1385c38fbde1978bd8e9bf4436ba46c
JimmiN <jimmi.nicastri@outlook.com>
2015-06-28 17:39:18 +1000

    Fix search page loading data from DB issues with sproc, multiple output params, and multipl result sets

--- search.php
+++ search.php
@@ -9,54 +9,69 @@
 
 		if(!$load)
 			die ("Loading HTML template has failed!");
-			
-		var_dump($load);
 		
 		$temp->setVariable("baseurl", BASE_URL);
-		$minYear = $maxYear = 0;
-		
-		
-		var_dump(DB_HOST);
-		var_dump(DB_NAME);
-		var_dump(DB_USER);
-		var_dump(DB_PWD);
+		$minYear = $maxYear = "";
 		
 		$conStr = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8";
-		
-		var_dump($conStr);
 		
 		// Getting Data
 		try{
 			$con = new PDO($conStr, DB_USER, DB_PWD);
-			$sql = 'CALL Search_BindData(@min,@max)';
-			$stmt = $con->query($sql);	
-			//$stmt = $con->execute();
-			//$stmt->closeCursor();
+			$records = $con->query('CALL Search_BindData(@min,@max)');
+			$setCount = 1;
 			
-			$out = $con->query("SELECT @min, @max")->fetch(PDO::FETCH_ASSOC);
+			// Looping through resut sets
+			do{	
+				$set = $records->fetchAll(PDO::FETCH_ASSOC);
+				
+				if($setCount == 1){
+					//Bind Region List
+					foreach($set as $row){
+						$temp->setVariable("regionName", $row["region_name"]);
+						$temp->addBlock("regionSelect");
+					}
+				}
+				else if($setCount == 2){
+					//Adding an 'All' selection for grape list
+					$temp->setVariable("grapeName", "All");
+					$temp->addBlock("grapeSelect");
+					//Bind Grape Variety List to DB Values
+					foreach($set as $row){
+						$temp->setVariable("grapeName", $row["variety"]);
+						$temp->addBlock("grapeSelect");
+					}
+				}
+				$setCount++;
+				
+			}while($records->nextRowset());
+
+		
+			$minYear = $con->query("SELECT @min")->fetchAll(PDO::FETCH_ASSOC);
+			$maxYear = $con->query("SELECT @max")->fetchAll(PDO::FETCH_ASSOC);
 			
-			if($out){
-				$minYear = $out['@min'];
-				$maxYear = $out['@max'];	
-			}
-			
-			//$stmt = $con->query('SELECT MAX(year) AS year FROM wine');
-			
-			//while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-			//	$maxYear = $row['year'];
-			//}
-			
-			var_dump($minYear);
-			var_dump($maxYear);
 		}
-		catch(Exception $ex){
-		//	die("An Error has occured whist attempting to access the DB", $ex->getMessage());
-			var_dump($ex);
+		catch(PDOException $ex){
+			// Redirect to Error here!
 		}
 		
+		// Adding 'All' options for year Ddl's
+		$temp->setVariable("minYearName", "All");
+		$temp->addBlock("yearMinSelect");
+		$temp->setVariable("maxYearName", "All");
+		$temp->addBlock("yearMaxSelect");
 		
+		//Populating year min & max from DB
+		$min = (int)$minYear[0]["@min"];
+		$max = (int)$maxYear[0]["@max"];
 		
-		
+		for($i = $min; $i < ($max + 1); $i++){
+			$temp->setVariable("minYearName", $i);
+			$temp->addBlock("yearMinSelect");
+			$temp->setVariable("maxYearName", $i);
+			$temp->addBlock("yearMaxSelect");
+		}
+
 		$temp->generateOutput();
 	}
 
